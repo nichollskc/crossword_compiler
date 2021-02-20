dee0fb54cbed1481284de02a0df6aa3e158363e8
Kath Nicholls <nichollskc@gmail.com>
2021-02-20 17:36:42 +0000

    Add basic tests

--- src/graph.rs
+++ src/graph.rs
@@ -1,9 +1,11 @@
+use log::{info,warn,debug,error};
 use std::collections::{HashSet,HashMap};
 
 pub mod graph {
     pub struct Graph;
 }
 
+#[derive(Debug)]
 struct Node {
     // Original ID given to the node
     node_id: usize,
@@ -28,6 +30,7 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
 struct Graph {
     // Node storage
     node_storage: Vec<Node>,
@@ -51,6 +54,7 @@ impl Graph {
 
     fn add_edges(&mut self, edges: Vec<(usize, usize)>) {
         for edge in edges.iter() {
+            debug!("Edge {:#?}", edge);
             let (first, second) = edge;
 
             // Check the nodes already exist, and add them if not
@@ -66,6 +70,7 @@ impl Graph {
     fn add_node(&mut self, node_id: usize) -> bool {
         let already_present: bool = self.node_map.contains_key(&node_id);
         if !already_present {
+            debug!("Adding node {}", node_id);
             // Create node and find the index it will have
             let node: Node = Node::new(node_id);
             let node_index: usize = self.num_nodes;
@@ -84,14 +89,42 @@ impl Graph {
             None => None,
         }
     }
+
+    fn count_edges(&self) -> usize {
+        let mut edge_count: usize = 0;
+        for node in self.node_storage.iter() {
+            edge_count += node.connected_nodes.len();
+        }
+        edge_count / 2
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn check_graph(graph: Graph, expected_nodes: usize, expected_edges: usize) {
+        println!("Result is {:#?}", graph);
+        assert_eq!(graph.num_nodes, expected_nodes);
+        assert_eq!(graph.node_storage.len(), expected_nodes);
+        assert_eq!(graph.count_edges(), expected_edges);
+    }
+
     #[test]
-    fn build_graph() {
-        Graph::new_from_edges(vec![(0, 1), (1, 2), (2, 3)]);
+    fn build_graph_complex() {
+        let graph = Graph::new_from_edges(vec![(0, 1), (1, 2), (2, 3), (2, 0)]);
+        check_graph(graph, 4, 4);
+
+        let graph = Graph::new_from_edges(vec![(0, 1), (0, 1), (0, 1), (1, 0)]);
+        check_graph(graph, 2, 1);
+    }
+
+    #[test]
+    fn build_graph_basic() {
+        let graph = Graph::new_from_edges(vec![(0, 1), (1, 2), (2, 3)]);
+        check_graph(graph, 4, 3);
+
+        let graph = Graph::new_from_edges(vec![(10, 11), (11, 20), (20, 5)]);
+        check_graph(graph, 4, 3);
     }
 }
