f146ab72192b90b2ff483cb44b1eba0152998b6e
Kath Nicholls <nichollskc@gmail.com>
2021-03-05 11:59:17 +0000

    Add basic command line parsing to main

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate clap;
+
+use clap::{App, Arg, SubCommand};
+
 use crossword;
 
 fn main() {
     crossword::logging::init_logger(true);
-    let mut generator = crossword::generator::CrosswordGenerator::new_from_file("tests/resources/input_with_clues.txt");
-    let results = generator.generate();
-    for grid in results.iter() {
-        println!("{}", grid.to_string());
-        let mut printer = crossword::grid::CrosswordPrinter::new(grid.clone());
-        printer.print_to_pdf("test");
+
+    let setting_names = ["num-per-gen", "num-children", "max-rounds", "seed", "moves-between-scores"];
+    let mut setting_args: Vec<Arg> = vec![];
+    for setting_name in setting_names.iter() {
+        let full_name = format!("--{}", setting_name);
+        setting_args.push(Arg::with_name(&setting_name).long(&setting_name).takes_value(true));
     }
+
+    let matches = App::new("Crossword pedigree")
+        .version("1.0")
+        .author("Kath Nicholls")
+        .about("Generates a crossword from a set of clues and answers")
+        .arg(Arg::with_name("CLUE_FILE")
+                .required(true))
+        .args(&setting_args)
+        .get_matches();
+
+    let mut settings_map: HashMap<&str, u32> = HashMap::new();
+    for setting_name in setting_names.iter() {
+        let setting_value = value_t!(matches, *setting_name, u32);
+        match setting_value {
+            Ok(value) => { settings_map.insert(setting_name, value); },
+            Err(error) if error.kind == clap::ErrorKind::ArgumentNotFound => (),
+            Err(error) => { panic!("Failed to parse arguments - invalid argument given. {}", error); },
+        }
+    }
+    println!("{:?}", settings_map);
+
+    let mut generator = crossword::generator::CrosswordGenerator::new_from_file(matches.value_of("CLUE_FILE").unwrap());
+
 }
